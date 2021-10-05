3afef2782416040497fdc80e5a4826e9d373347e
Noormohammad011 <noormohammad.net1503011@gmail.com>
2021-10-05 13:45:56 +0600

    context api is completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react'
+import { useAsync } from 'react-async-hook'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Layout from './layout/Layout'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ContactUsPage from './pages/ContactUsPage'
-import FaqPage from './pages/FaqPage';
-import NotFoundPage from './pages/NotFoundPage';
+import FaqPage from './pages/FaqPage'
+import NotFoundPage from './pages/NotFoundPage'
+
+//Create Context
+export const CourseContext = React.createContext()
+//Fetch
+const fetchCourses = async () => (await fetch('./courses.JSON')).json()
+
 const App = () => {
+  const fetchCoursesFromExternal = useAsync(fetchCourses)
+
   return (
     <Router>
-      <Layout>
-        <Switch>
-          <Route path='/' component={HomePage} exact />
-          <Route path='/about' component={AboutPage} exact />
-          <Route path='/contact' component={ContactUsPage} exact />
-          <Route path='/faq' component={FaqPage} exact />
-          <Route path='*' component={NotFoundPage} exact />
-        </Switch>
-      </Layout>
+      <CourseContext.Provider
+        value={{
+          fetchCoursesFromExternal,
+        }}
+      >
+        <Layout>
+          <Switch>
+            <Route path='/' component={HomePage} exact />
+            <Route path='/about' component={AboutPage} exact />
+            <Route path='/contact' component={ContactUsPage} exact />
+            <Route path='/faq' component={FaqPage} exact />
+            <Route path='*' component={NotFoundPage} exact />
+          </Switch>
+        </Layout>
+      </CourseContext.Provider>
     </Router>
   )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,29 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import Courses from '../components/Courses'
-const HomePage = () => {
-  const [courses, setCourses] = useState([])
+import React, { useContext } from 'react'
 
-  useEffect(() => {
-    fetch('./courses.JSON')
-      .then((res) => res.json())
-      .then((data) => setCourses(data))
-  }, [])
-  console.log(courses.html)
+import { CourseContext } from '../App'
+import Courses from '../components/Courses'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+
+const HomePage = () => {
+
+  const { fetchCoursesFromExternal } = useContext(CourseContext)
+  const { result, error, loading } = fetchCoursesFromExternal
 
   return (
     <>
+      {loading && <Loader />}
+      {error && <Message variant='danger'>{error.message}</Message>}
       <div className='container'>
-        <h1 className='text-left slideInRight'>Html</h1>
+        <h1 className='text-left slideInRight'>HTML</h1>
         <div className='row row-cols-1 row-cols-sm-2 row-cols-md-4'>
-          {courses &&
-            courses?.html?.map((c, i) => <Courses course={c} key={i} />)}
+          {result &&
+            result?.html?.map((c, i) => <Courses course={c} key={i} />)}
         </div>
       </div>
       <div className='container'>
-        <h1 className='text-left slideInRight'>Css</h1>
+        <h1 className='text-left slideInRight'>CSS</h1>
         <div className='row row-cols-1 row-cols-sm-2 row-cols-md-4'>
-          {courses &&
-            courses?.css?.map((c, i) => <Courses course={c} key={i} />)}
+          {result && result?.css?.map((c, i) => <Courses course={c} key={i} />)}
         </div>
       </div>
     </>
